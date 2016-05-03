9cce7d267d66ed49e0cb02cb3f4e52c5487f67f3
unknown <vitalyki@vitalyki-u.nice.com>
2016-05-03 13:44:17 +0300

    some UI draft

---
+++ ZIP Archivator/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace ZIP_Archivator
+{
+    partial class FormZipArchivator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.commandsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonExtractTo = new System.Windows.Forms.Button();
+            this.buttonTest = new System.Windows.Forms.Button();
+            this.buttonView = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonFind = new System.Windows.Forms.Button();
+            this.buttonInfo = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.commandsToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(833, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // commandsToolStripMenuItem
+            // 
+            this.commandsToolStripMenuItem.Name = "commandsToolStripMenuItem";
+            this.commandsToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.commandsToolStripMenuItem.Text = "Commands";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 8;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 76F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 76F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 76F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 76F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 76F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 76F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 76F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.buttonAdd, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonExtractTo, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonTest, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonView, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonDelete, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonFind, 5, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonInfo, 6, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 27);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(831, 74);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonAdd.Image = global::ZIP_Archivator.Properties.Resources.Archive_icon;
+            this.buttonAdd.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonAdd.Location = new System.Drawing.Point(3, 7);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(70, 60);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonExtractTo
+            // 
+            this.buttonExtractTo.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonExtractTo.Image = global::ZIP_Archivator.Properties.Resources.Extract_object_icon;
+            this.buttonExtractTo.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonExtractTo.Location = new System.Drawing.Point(79, 7);
+            this.buttonExtractTo.Name = "buttonExtractTo";
+            this.buttonExtractTo.Size = new System.Drawing.Size(70, 60);
+            this.buttonExtractTo.TabIndex = 3;
+            this.buttonExtractTo.TabStop = false;
+            this.buttonExtractTo.Text = "Extract To";
+            this.buttonExtractTo.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonExtractTo.UseVisualStyleBackColor = true;
+            // 
+            // buttonTest
+            // 
+            this.buttonTest.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonTest.Image = global::ZIP_Archivator.Properties.Resources.Tests_icon;
+            this.buttonTest.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonTest.Location = new System.Drawing.Point(155, 7);
+            this.buttonTest.Name = "buttonTest";
+            this.buttonTest.Size = new System.Drawing.Size(70, 60);
+            this.buttonTest.TabIndex = 4;
+            this.buttonTest.Text = "Test";
+            this.buttonTest.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonTest.UseVisualStyleBackColor = true;
+            // 
+            // buttonView
+            // 
+            this.buttonView.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonView.Image = global::ZIP_Archivator.Properties.Resources.search_icon;
+            this.buttonView.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonView.Location = new System.Drawing.Point(231, 7);
+            this.buttonView.Name = "buttonView";
+            this.buttonView.Size = new System.Drawing.Size(70, 60);
+            this.buttonView.TabIndex = 5;
+            this.buttonView.Text = "View";
+            this.buttonView.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonView.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonDelete.Image = global::ZIP_Archivator.Properties.Resources.edit_delete_icon;
+            this.buttonDelete.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonDelete.Location = new System.Drawing.Point(307, 7);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(70, 60);
+            this.buttonDelete.TabIndex = 6;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // buttonFind
+            // 
+            this.buttonFind.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonFind.Image = global::ZIP_Archivator.Properties.Resources.Symbols_Find_icon;
+            this.buttonFind.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonFind.Location = new System.Drawing.Point(383, 7);
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.Size = new System.Drawing.Size(70, 60);
+            this.buttonFind.TabIndex = 7;
+            this.buttonFind.Text = "Find";
+            this.buttonFind.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonFind.UseVisualStyleBackColor = true;
+            // 
+            // buttonInfo
+            // 
+            this.buttonInfo.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonInfo.Image = global::ZIP_Archivator.Properties.Resources.Symbols_Info_icon;
+            this.buttonInfo.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonInfo.Location = new System.Drawing.Point(459, 7);
+            this.buttonInfo.Name = "buttonInfo";
+            this.buttonInfo.Size = new System.Drawing.Size(70, 60);
+            this.buttonInfo.TabIndex = 8;
+            this.buttonInfo.Text = "Info";
+            this.buttonInfo.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonInfo.UseVisualStyleBackColor = true;
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(0, 108);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(536, 20);
+            this.textBoxAddress.TabIndex = 2;
+            // 
+            // FormZipArchivator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 480);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormZipArchivator";
+            this.Text = "ZIP Archivator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem commandsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonExtractTo;
+        private System.Windows.Forms.Button buttonTest;
+        private System.Windows.Forms.Button buttonView;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonFind;
+        private System.Windows.Forms.Button buttonInfo;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.DirectoryServices.DirectoryEntry directoryEntry1;
+    }
+}
+
---
+++ ZIP Archivator/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+
+namespace ZIP_Archivator
+{
+    public partial class FormZipArchivator : Form
+    {
+        public FormZipArchivator()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+    }
+}
